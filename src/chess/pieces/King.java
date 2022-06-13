a4d0f15fa657344fa195b57a18620f40833b3919
Thallys Christian <tchristian.ucp@gmail.com>
2022-06-13 19:59:55 -0300

    Pawn

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -5,11 +5,11 @@ import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-public class King extends ChessPiece {
+public class King extends ChessPiece{
 
 	public King(Board board, Color color) {
 		super(board, color);
-		// TODO Auto-generated constructor stub
+	
 	}
 
 	@Override
